c9949f5421e83952af9122280e698b0969651c9b
Scott Deeter <scottd@knowclassic.com>
2014-08-04 21:15:47 -0400

    Added collection2 package

--- collections/posts.js
+++ collections/posts.js
@@ -1,20 +1,22 @@
+postsSchemaObject = {
+  title: {
+    type: String,
+    max: 100
+  },
+  price: {
+    type: Number,
+    max: 8
+  },
+  description: {
+    type: String,
+    max: 250
+  },
+  type: {
+    type: String,
+    max: 100
+  }
+};
+
 Posts = new Meteor.Collection('posts', {
-  schema: new SimpleSchema({
-    title: {
-      type: String,
-      max: 100
-    },
-    price: {
-      type: Number,
-      max: 8
-    },
-    description: {
-      type: String,
-      max: 250
-    },
-    type: {
-      type: String,
-      max: 100
-    }
-  })
+  schema: new SimpleSchema(postsSchemaObject)
 });
